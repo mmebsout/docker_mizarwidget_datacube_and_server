1ef9451c422ed378931692debf0a368191625a64
mmebsout <magalimebsout+dev@gmail.com>
2020-04-03 14:32:29 +0200

    Download demo file on run

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+WORKSPACE_PATH=$PWD/datacube_workspace
+mkdir -p $WORKSPACE_PATH/private && mkdir $WORKSPACE_PATH/public
+chmod +x $WORKSPACE_PATH/private && chmod +x $WORKSPACE_PATH/public
+
+wget -P  $WORKSPACE_PATH/public http://idoc-herschel.ias.u-psud.fr/sitools/datastorage/user/storageRelease//R7_spire_fts/HIPE_Fits/FTS_SPIRE/OT1_atielens/M17-2/1342228703_M17-2_SPIRE-FTS_15.0.3244_HR_SLW_gridding_cube.fits
+
 docker run --name mizarwidget-datacube_and_server \
             -d \
             -p 8000:80 \
