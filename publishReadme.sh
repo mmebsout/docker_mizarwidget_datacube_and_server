cfb3cb00662d86cbe2272ab5432aa2726f7fcc69
mmebsout <magalimebsout+dev@gmail.com>
2020-04-03 14:32:43 +0200

    Fix username

--- publishReadme.sh
+++ publishReadme.sh
@@ -6,7 +6,7 @@ fi
 
 docker run --rm \
     -v $PWD/DockerHub_Readme.md:/data/README.md \
-    -e DOCKERHUB_USERNAME='idocias' \
+    -e DOCKERHUB_USERNAME='idocdocker' \
     -e DOCKERHUB_PASSWORD=$idocdockerpassword \
     -e DOCKERHUB_REPO_PREFIX='idocias' \
     -e DOCKERHUB_REPO_NAME='mizarwidget-datacube-and-server' \
