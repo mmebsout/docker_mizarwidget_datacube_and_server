f12952fa458e1b26ada74f3ab180f49dbb67ca87
mmebsout <magalimebsout+dev@gmail.com>
2020-05-19 15:48:45 +0200

    fixing demo file
    Using the right file for the given position

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ WORKSPACE_PATH=$PWD/datacube_workspace
 mkdir -p $WORKSPACE_PATH/private && mkdir $WORKSPACE_PATH/public
 chmod +x $WORKSPACE_PATH/private && chmod +x $WORKSPACE_PATH/public
 
-wget -P  $WORKSPACE_PATH/public http://idoc-herschel.ias.u-psud.fr/sitools/datastorage/user/storageRelease//R7_spire_fts/HIPE_Fits/FTS_SPIRE/OT1_atielens/M17-2/1342228703_M17-2_SPIRE-FTS_15.0.3244_HR_SLW_gridding_cube.fits
+wget -P  $WORKSPACE_PATH/public http://idoc-herschel.ias.u-psud.fr/sitools/datastorage/user/storageRelease/R6_pacs_spectro/HIPE_Fits/SPECTRO_PACS/SAG-4/HH_IR_int-2/HH_IR_int-2_1342228508_L2_red_145.52_OI3P0-3P1_ProjectedCube.fits
 
 docker run --name mizarwidget-datacube-and-server \
             -d \
